a60f28180aaf5f5487210195b2b414e7c24db49b
vinit-chauhan <chauhanvinit23@gmail.com>
2024-01-25 01:35:19 -0500

    Variables in Rust

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    /*
-       Multi line
-       comment
-    */
-    println!("Hello, world!"); // single line comment
+    let y: i32 = 11222; // immutable variable.
+    let mut x: i8 = 100; // mutable variable
+
+    println!("y: {}", y);
+    println!("x: {}", x);
+    x = 20; // gives error if mut is not used.
+    println!("new x: {}", x);
 }

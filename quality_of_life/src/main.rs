70dc6150be90a7152c870994c1ba9c80a6e79d76
vinit-chauhan <chauhanvinit23@gmail.com>
2024-02-03 14:35:29 -0500

    Added code for todo macro

---
+++ quality_of_life/src/main.rs
@@ -0,0 +1,40 @@
+struct Person {}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct MyMoves {
+    name: String,
+    energy_used_pr: u8,
+}
+
+trait Dance {
+    fn pick_the_song(&self, name: String) -> bool;
+    fn dance(&self, name: String, energy_used_pr: u8) -> MyMoves;
+}
+
+impl Dance for Person {
+    fn dance(&self, name: String, energy_used_pr: u8) -> MyMoves {
+        MyMoves {
+            name,
+            energy_used_pr,
+        }
+    }
+    fn pick_the_song(&self, _name: String) -> bool {
+        // removes compile time error for un-implemented code.
+        // panics if someone uses that code.
+        todo!();
+    }
+}
+
+fn main() {
+    let person = Person {};
+
+    println!(
+        "dance move: {:?}",
+        person.dance(String::from("Jump"), 10_u8)
+    );
+    println!(
+        "dance move: {:?}",
+        person.pick_the_song(String::from("some song"))
+    );
+}
